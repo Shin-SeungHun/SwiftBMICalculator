9cf170d88f2c0cdfc2e0fc4b4c900fb5b3050e4a
shin-SeungHun <sng7387@gmail.com>
2024-04-04 16:51:41 +0900

    reafctor: 결과 화면에 키와 몸무게 추가, bmi값에 따른 비만도 추가, 아무것도 입력하지 않았을때 스낵바 표시

--- BMICalculator/ContentView.swift
+++ BMICalculator/ContentView.swift
@@ -11,22 +11,30 @@ struct ContentView: View {
     @State private var height: String = ""
     @State private var weight: String = ""
     @State private var isShowingResult: Bool = false
+    @State private var isInvalidInput: Bool = false // 입력이 잘못되었는지를 나타내는 상태 변수
     
     var body: some View {
         NavigationView {
             VStack {
-                TextField("Height (cm)", text: $height)
+                TextField("키 (cm)", text: $height)
                     .padding()
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad) // 숫자 입력용 키보드 사용
                 
-                TextField("Weight (kg)", text: $weight)
+                TextField("몸무게 (kg)", text: $weight)
                     .padding()
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad) // 숫자 입력용 키보드 사용
                 
                 Button(action: {
-                    isShowingResult = true
+                    if (Double(height) != nil), (Double(weight) != nil) {
+                        isShowingResult = true
+                        isInvalidInput = false // 유효한 입력이 있을 때는 스낵바를 표시하지 않도록 설정
+                    } else {
+                        isInvalidInput = true // 유효하지 않은 입력일 때 스낵바를 표시
+                    }
                 }) {
-                    Text("계산")
+                    Text("BMI 계산")
                         .padding()
                         .frame(maxWidth: .infinity)
                         .background(Color.blue)
@@ -36,6 +44,9 @@ struct ContentView: View {
                 .sheet(isPresented: $isShowingResult, content: {
                     ResultView(height: Double(height) ?? 0, weight: Double(weight) ?? 0)
                 })
+                .alert(isPresented: $isInvalidInput) {
+                    Alert(title: Text("유효하지 않은 값"), message: Text("올바른 값을 입력하세요."), dismissButton: .default(Text("확인")))
+                }
                 
                 Spacer()
             }
--- BMICalculator/ResultView.swift
+++ BMICalculator/ResultView.swift
@@ -13,14 +13,42 @@ struct ResultView: View {
     
     var body: some View {
         let bmi = calculateBMI()
-        return Text("BMI: \(String(format: "%.2f", bmi))")
-            .padding()
-    }
+        let bmiCategory = calcBmiCategory(bmi)
+            return VStack {
+                Text("키: \(String(format: "%.1f", Double(height))) cm / 몸무게: \(String(format: "%.1f", Double(weight))) kg")
+                    .padding()
+                
+                Text("BMI: \(String(format: "%.2f", bmi))")
+                    .padding()
+                    
+                Text("비만도: \(bmiCategory)")
+                    .padding()
+                    
+                }
+            }
     
-    func calculateBMI() -> Double {
+    private func calculateBMI() -> Double {
         let heightInMeters = height / 100
         return weight / (heightInMeters * heightInMeters)
     }
+    
+    private func calcBmiCategory(_ bmi: Double) -> String {
+            var result = "저체중"
+
+            if bmi >= 35 {
+                result = "고도 비만"
+            } else if bmi >= 30 {
+                result = "2단계 비만"
+            } else if bmi >= 25 {
+                result = "1단계 비만"
+            } else if bmi >= 23 {
+                result = "과체중"
+            } else if bmi >= 18.5 {
+                result = "정상"
+            }
+            
+            return result
+        }
 }
 
 #Preview {
